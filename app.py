e714df74f63617dbb05eace49a06708741e5fbad
bangsuwan <angsulwan@gmail.com>
2026-03-31 21:23:22 +0700

    update: app

--- app.py
+++ app.py
@@ -4,8 +4,7 @@ import time
 from joblib import load
 
 st.set_page_config(
-    page_title="Aplikasi Risk Assessment Monitoring Kelayakan Air",
-    page_icon="💧",
+    page_title="Dropout Risk Assessment App",
     layout="centered",
 )
 
@@ -32,42 +31,6 @@ def run_inferece(data_input: pd.DataFrame) -> dict:
     potability_probs = CLF_MODEL.predict_proba(X)[0, 1]
     potability_pred = int(potability_probs >= THRESHOLD_POTABILITY)
 
-    # anomaly detection
-    anom_flag = ANOM_MODEL.predict(X)[0] # 1: normal, -1: anomali
-    is_anom = int(anom_flag == -1) # 0: normal, 1: anomali
-
-    # risk assessment
-    if potability_pred == 1 and is_anom == 0:
-        risk_label = "AMAN"
-        recommendation = (
-            "Air layak konsumsi dan sesuai pola normal." 
-            "Dapat didistribusikan dengan monitoring rutin."
-        )
-    
-    elif potability_pred == 1 and is_anom == 1:
-        risk_label = "PERLU DICEK"
-        recommendation = (
-            "Air layak konsumsi, namun polanya tidak umum."
-            "Disarankan untuk melakukan pengecekan lebih lanjut."
-        )
-    
-    elif potability_pred == 0 and is_anom == 0:
-        risk_label = "TIDAK LAYAK"
-        recommendation = (
-            "Air tidak layak konsumsi dan pola normal."
-            "Perlu optimasi proses pengolahan air dan pengecekan lebih lanjut."
-        )
-    else: # potability_pred == 0 and is_anom == 1:
-        risk_label = "KRITIS"
-        recommendation = (
-            "Air sangat tidak layak konsumsi dan terdeteksi anomali ekstrim."
-            "Wajib verifikasi laboratorium dan penanganan darurat."
-        )
-    return {
-        "risk_label": risk_label,
-        "recommendation": recommendation
-    }
-
 # Streamlit UI
 st.title("Aplikasi Risk Assessment Monitoring Kelayakan Air 💧")
 st.markdown("Masukkan data setiap parameter kualitas air untuk melakukan Risk Assessment Kelayakan Air**!")
@@ -89,21 +52,59 @@ columns = [
 ]
 
 ## Input data numerik
-ph = st.number_input("ph", min_value=0.1, max_value=14.0, value=7.0)
-hardness = st.number_input("Hardness", min_value=0.1, max_value=1000.0, value=200.0)
-solids = st.number_input("Solids", min_value=0.1, max_value=100000.0, value=20000.0)
-chloramines = st.number_input("Chloramines", min_value=0.1, max_value=100.0, value=7.0)
-sulfate = st.number_input("Sulfate", min_value=0.1, max_value=1000.0, value=300.0)
-conductivity = st.number_input("Conductivity", min_value=0.1, max_value=1000.0, value=400.0)
-organic_carbon = st.number_input("Organic_carbon", min_value=0.1, max_value=1000.0, value=15.0)
-trihalomethanes = st.number_input("Trihalomethanes", min_value=0.1, max_value=1000.0, value=80.0)
-turbidity = st.number_input("Turbidity", min_value=0.1, max_value=100.0, value=4.0)
+marital_status = st.number_input("ph", min_value=0.1, max_value=14.0, value=7.0)
+application_mode = st.number_input("Hardness", min_value=0.1, max_value=1000.0, value=200.0)
+application_order = st.number_input("Solids", min_value=0.1, max_value=100000.0, value=20000.0)
+course = st.number_input("Chloramines", min_value=0.1, max_value=100.0, value=7.0)
+attendance = st.number_input("Sulfate", min_value=0.1, max_value=1000.0, value=300.0)
+prev_qual = st.number_input("Conductivity", min_value=0.1, max_value=1000.0, value=400.0)
+prev_qual_grade = st.number_input("Organic_carbon", min_value=0.1, max_value=1000.0, value=15.0)
+nationality = st.number_input("Trihalomethanes", min_value=0.1, max_value=1000.0, value=80.0)
+mothers_qual = st.number_input("Turbidity", min_value=0.1, max_value=100.0, value=4.0)
+fathers_qual = st.number_input("ph", min_value=0.1, max_value=14.0, value=7.0)
+mothers_occu = st.number_input("Hardness", min_value=0.1, max_value=1000.0, value=200.0)
+fathers_occu = st.number_input("Solids", min_value=0.1, max_value=100000.0, value=20000.0)
+admission_grade = st.number_input("Chloramines", min_value=0.1, max_value=100.0, value=7.0)
+displaced = st.number_input("Sulfate", min_value=0.1, max_value=1000.0, value=300.0)
+edu_special_needs = st.number_input("Conductivity", min_value=0.1, max_value=1000.0, value=400.0)
+debtor = st.number_input("Organic_carbon", min_value=0.1, max_value=1000.0, value=15.0)
+tuition_uptodate = st.number_input("Trihalomethanes", min_value=0.1, max_value=1000.0, value=80.0)
+gender = st.number_input("Turbidity", min_value=0.1, max_value=100.0, value=4.0)
+scholarship_holder = st.number_input("Hardness", min_value=0.1, max_value=1000.0, value=200.0)
+age_at_enroll = st.number_input("Solids", min_value=0.1, max_value=100000.0, value=20000.0)
+international = st.number_input("Chloramines", min_value=0.1, max_value=100.0, value=7.0)
+
+curr_units_1_credited = st.number_input("Sulfate", min_value=0.1, max_value=1000.0, value=300.0)
+curr_units_1_enrolled = st.number_input("Sulfate", min_value=0.1, max_value=1000.0, value=300.0)
+curr_units_1_eval = st.number_input("Sulfate", min_value=0.1, max_value=1000.0, value=300.0)
+curr_units_1_approved = st.number_input("Sulfate", min_value=0.1, max_value=1000.0, value=300.0)
+curr_units_1_grade = st.number_input("Sulfate", min_value=0.1, max_value=1000.0, value=300.0)
+curr_units_1_without_eval = st.number_input("Sulfate", min_value=0.1, max_value=1000.0, value=300.0)
+
+curr_units_2_credited = st.number_input("Sulfate", min_value=0.1, max_value=1000.0, value=300.0)
+curr_units_2_enrolled = st.number_input("Sulfate", min_value=0.1, max_value=1000.0, value=300.0)
+curr_units_2_eval = st.number_input("Sulfate", min_value=0.1, max_value=1000.0, value=300.0)
+curr_units_2_approved = st.number_input("Sulfate", min_value=0.1, max_value=1000.0, value=300.0)
+curr_units_2_grade = st.number_input("Sulfate", min_value=0.1, max_value=1000.0, value=300.0)
+curr_units_2_without_eval = st.number_input("Sulfate", min_value=0.1, max_value=1000.0, value=300.0)
+
+unemployement_rate = st.number_input("Conductivity", min_value=0.1, max_value=1000.0, value=400.0)
+inflation_rate = st.number_input("Organic_carbon", min_value=0.1, max_value=1000.0, value=15.0)
+gdp = st.number_input("Trihalomethanes", min_value=0.1, max_value=1000.0, value=80.0)
 
 # Menyimpan data ke dalam DataFrame
 data_input = pd.DataFrame([[
-    ph, hardness, solids, 
-    chloramines, sulfate, conductivity, 
-    organic_carbon, trihalomethanes, turbidity
+    marital_status, application_mode, application_order, course,
+    attendance, prev_qual, prev_qual_grade, nationality, 
+    mothers_qual, fathers_qual, mothers_occu, fathers_occu, 
+    admission_grade, displaced, edu_special_needs, 
+    debtor, tuition_uptodate, gender, scholarship_holder, 
+    age_at_enroll, international,
+    curr_units_1_credited, curr_units_1_enrolled, curr_units_1_eval,
+    curr_units_1_approved, curr_units_1_grade, curr_units_1_without_eval,
+    curr_units_2_credited, curr_units_2_enrolled, curr_units_2_eval,
+    curr_units_2_approved, curr_units_2_grade, curr_units_2_without_eval,
+    unemployement_rate, inflation_rate, gdp
 ]], columns=columns)
 
 # Tombol untuk menampilkan data
